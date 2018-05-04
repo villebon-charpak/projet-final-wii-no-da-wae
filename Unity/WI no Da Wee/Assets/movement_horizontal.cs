f6030fe1492981c459b368e604e7db13513b1503
Quentin Lieumont <quentin.lieumont@gmail.com>
2018-05-04 13:42:38 +0200

    Ajout Unity
    On ajoute UNITY
    C KAKA

---
+++ Unity/WI no Da Wee/Assets/movement_horizontal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_horizontal : MonoBehaviour {
+	float x=0;
+	void Start()
+	{
+		transform.position = new Vector3(x, -2.5f, -3f);
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (x*0.1f, -2.5f, -3f);
+		x = moveHorizontal+ x;
+		transform.position = movement;
+
+	}
+}
