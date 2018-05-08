6340356b56bbfe411dc8d44ccf20e8bfef6f8e7f
Quentin Lieumont <quentin.lieumont@gmail.com>
2018-05-08 21:32:05 +0200

    Update Unity
    Supression puis reupload du fichier de colaboration de Unity
    
    CET UPLOAD COMME LES AUTRES EN REPRESENTE PAS NOTRE PART DU TRAVAIL, POUR JUGER DE LA PARTICIPATION DE CHAQUE UNS SE REFERER AU FICHIER README DE LA BRANCHE MASTER

---
+++ WI no Da Wee/Assets/Script/click.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+	void OnGUI() {
+		Event e = Event.current;
+		GameObject cam = GameObject.Find("Main Camera");
+		float poscam = cam.transform.position.x;
+		double xmap = (e.mousePosition.x*(26+poscam)/810)-13+poscam;
+		double ymap = -((e.mousePosition.y * 21/315)-10.5);
+		//Debug.Log(e.mousePosition);
+		if (e.button == 1)
+		{
+			//Debug.Log (xmap.ToString()+"   "+ymap.ToString());
+		}
+		if (e.button == 1 && -4 <= xmap && xmap <= 0 && ymap <= 0){
+			Debug.Log("Right Click");
+			cam.transform.position = new Vector3(0f, 0f, 2f);
+		}
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ WI no Da Wee/Assets/Script/initSol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initSol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject sol = new GameObject();
+		sol.AddComponent<SpriteRenderer>();
+		sol.name = "Sol";
+		var result = Resources.Load<Sprite>("Sol_Terre");
+		sol.GetComponent<SpriteRenderer> ().sprite = result;
+		sol.transform.position = new Vector3(0f,-5.3f,0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
